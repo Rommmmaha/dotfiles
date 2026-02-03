c4243e402c4be0a56284f6ad1f1b013cdf89a2bd
Rommmmaha <Rommmmaha@gmail.com>
2026-02-03 20:03:32 +0200

    Fix packages | Change script padding

--- run_after_my-utils.bash
+++ run_after_my-utils.bash
@@ -10,27 +10,27 @@ VERSION_FILE="$INSTALL_DIR/.my_tools_last_id"
 
 set -e
 if ! curl -s --head --max-time 2 https://api.github.com > /dev/null; then
-    echo "Offline: Skipping update check."
-    exit 0
+  echo "Offline: Skipping update check."
+  exit 0
 fi
 API_URL="https://api.github.com/repos/$GITHUB_USER/$REPO_NAME/releases?per_page=1"
 RELEASE_JSON=$(curl -s "$API_URL")
 if [[ $(echo "$RELEASE_JSON" | jq 'type == "array"' 2>/dev/null) != "true" ]]; then
-    exit 0
+  exit 0
 fi
 if [[ $(echo "$RELEASE_JSON" | jq 'length') -eq 0 ]]; then
-    exit 0
+  exit 0
 fi
 REMOTE_ID=$(echo "$RELEASE_JSON" | jq -r '.[0].id')
 TAG_NAME=$(echo "$RELEASE_JSON" | jq -r '.[0].tag_name')
 DOWNLOAD_URL=$(echo "$RELEASE_JSON" | jq -r ".[0].assets[] | select(.name==\"$BINARY_ARCHIVE_NAME\") | .browser_download_url")
 if [ -z "$DOWNLOAD_URL" ] || [ "$DOWNLOAD_URL" == "null" ]; then
-    exit 0
+  exit 0
 fi
 LOCAL_ID=""
 [ -f "$VERSION_FILE" ] && LOCAL_ID=$(cat "$VERSION_FILE")
 if [ "$REMOTE_ID" == "$LOCAL_ID" ]; then
-    exit 0
+  exit 0
 fi
 echo "Updating $REPO_NAME to $TAG_NAME..."
 TMP_DIR=$(mktemp -d)
