150bb7a205bbcd2b2888fde51072bdc36d26a7d9
Rommmmaha <Rommmmaha@gmail.com>
2026-06-15 10:47:59 +0300

    fix rework

--- .chezmoiscripts/run_after_98-unpack-cursor.bash
+++ .chezmoiscripts/run_after_98-unpack-cursor.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ ! -d "$HOME/.icon/GoogleDot-Black" ]; then
+if [ ! -d "$HOME/.icons/GoogleDot-Black" ]; then
     mkdir -p "$HOME/.icons"
     tar -xzf "$HOME/.local/share/chezmoi/.archives/GoogleDot-Black.tar.gz" -C "$HOME/.icons"
 fi
