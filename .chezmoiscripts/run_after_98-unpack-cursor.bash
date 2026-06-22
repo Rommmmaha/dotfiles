bfdf18d882479b9102b769c315c3c977dcdbea9c
Rommmmaha <Rommmmaha@gmail.com>
2026-06-22 20:56:21 +0300

    fix: broken cursor symlinks

--- .chezmoiscripts/run_after_98-unpack-cursor.bash
+++ .chezmoiscripts/run_after_98-unpack-cursor.bash
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-if [ ! -d "$HOME/.icons/GoogleDot-Black" ]; then
-    mkdir -p "$HOME/.icons"
-    tar -xzf "$HOME/.local/share/chezmoi/.archives/GoogleDot-Black.tar.gz" -C "$HOME/.icons"
-fi
+mkdir -p "$HOME/.icons"
+tar -xzf "$HOME/.local/share/chezmoi/.archives/GoogleDot-Black.tar.gz" -C "$HOME/.icons"
